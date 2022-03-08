dd0c97c4a1bbc8a845fc37f1ff1a2d9beabe961e
Koseungyu <98968060+Koseungyu@users.noreply.github.com>
2022-03-08 20:25:47 +0900

    Update login.py

--- MiniPR/main/login.py
+++ MiniPR/main/login.py
@@ -1,11 +1,37 @@
 from main import * #from main import * : main에 선언된 모든 값을 가져온다 , __init__ file에 선언된 라이브러리를 가져와 사용할 수 있음.
 from flask import Blueprint
+import jwt
+import datetime
+import hashlib
+
 
 #객체 = Blueprint("name" , __name__ , url_prefix="") : (이름, 모듈명, URL 프리픽스 값)
 #이름은 url_for함수에서 사용되며 , 모듈명은 init.py에서 선언한 모듈을 뜻함 , url_prefix="주소"의 주소에는 브라우져에서 선언될 url을 입력한다 
 
 blueprint = Blueprint("login" , __name__ , url_prefix="/login")
 
+SECRET_KEY = 'CAR'
+
+
 @blueprint.route("/") #<- 데코레이터
 def login_template():
     return render_template("login.html")
+
+    # 로그인
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+
+    pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    result = db.users.find_one({'username': username_receive, 'password': pw_hash})
+
+    if result is not None:
+        payload = {
+         'id': username_receive,
+         'exp': datetime.utcnow() + timedelta(seconds= 60 * 60 * 24)  # 로그인 24시간 유지
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256').decode('utf-8')
+
+        return jsonify({'result': 'success', 'token': token})
+    # 찾지 못하면
+    else:
+        return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
